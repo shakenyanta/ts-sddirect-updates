83e4e5baecbfb5496490dda575a41d1da4cd749c
shakenyanta <ichiro.enoki@gmail.com>
2026-04-02 00:10:20 +0900

    ■v0.3.12

---
+++ copyScript.bat
@@ -0,0 +1,3 @@
+powershell -Command "Copy-Item '..\ts-sddirect\dist\ts-sddirect-setup.exe' ."
+powershell -Command "Copy-Item '..\ts-sddirect\dist\ts-sddirect-setup.exe.blockmap' ."
+powershell -Command "Copy-Item '..\ts-sddirect\dist\latest.yml' ."
